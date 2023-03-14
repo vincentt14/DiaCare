5f74fae5c4e837236f2b4d2dd9f5eed1900c745b
Vincent <vincent14@student.umn.ac.id>
2023-03-14 17:18:39 +0700

    fix not found page

--- resources/views/pages/pageNotFound.blade.php
+++ resources/views/pages/pageNotFound.blade.php
@@ -4,11 +4,13 @@
   <section class="pt-28 pb-24 lg:pt-36 lg:pb-10">
     <div class="container">
       <div class="w-full self-center px-4">
-        <h1 class="font-bold text-primary text-4xl lg:text-5xl">Sorry 🙁, this Page not Found</h1>
-        <h2 class="font-light text-primary text-lg mb-5 lg:text-2xl mt-2">Go back to Home Page to see more options
+        <h1 class="text-4xl font-bold text-primary lg:text-5xl">Sorry 🙁, this Page not Found</h1>
+        <h2 class="mb-5 mt-2 text-lg font-light text-primary lg:text-2xl">Go back to Home Page to see more options
         </h2>
       </div>
-      <Lottie options={defaultOptions} height={300} width={300} />
+      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+      <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_ilwhiuo7.json" background="transparent"
+        speed="1" style="width: 300px; height: 300px;" loop autoplay></lottie-player>
     </div>
   </section>
 @endsection
