762e9e22ca06fce0f8ac8adf1ecd679df8fb909e
Vincent <vincent14@student.umn.ac.id>
2023-04-01 22:59:02 +0700

    editing user and diseases tables

--- database/migrations/2023_03_31_073036_create_diseases_table.php
+++ database/migrations/2023_03_31_073036_create_diseases_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->timestamps();
             $table->string('diseases_code');
             $table->string('diseases');
+            $table->string('type');
+            $table->text('description');
         });
     }
 
