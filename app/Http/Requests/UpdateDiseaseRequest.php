d3335852cccb160a41b9cfb668b7e02b28a45ec6
Vincent <vincent14@student.umn.ac.id>
2023-04-24 21:10:25 +0700

    fixing request on medicine and disease

--- app/Http/Requests/UpdateDiseaseRequest.php
+++ app/Http/Requests/UpdateDiseaseRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreSymptomRequest extends FormRequest
+class UpdateDiseaseRequest extends FormRequest
 {
   /**
    * Determine if the user is authorized to make this request.
