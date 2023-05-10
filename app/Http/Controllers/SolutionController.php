443474f762191b0aee28ba29c605e58f617523a2
Vincent <vincent14@student.umn.ac.id>
2023-05-10 13:41:09 +0700

    adding Solutions MVC

---
+++ app/Http/Controllers/SolutionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Solution;
+use App\Http\Requests\StoreSolutionRequest;
+use App\Http\Requests\UpdateSolutionRequest;
+
+class SolutionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSolutionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Solution $solution)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Solution $solution)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSolutionRequest $request, Solution $solution)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Solution $solution)
+    {
+        //
+    }
+}
